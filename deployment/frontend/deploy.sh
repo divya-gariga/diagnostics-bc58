a70e326ccae3866e6579322244e48e18a05e1333
divya-gariga <divya.gariga@zemosolabs.com>
2022-10-24 12:28:09 +0530

    fe deploy

---
+++ deployment/frontend/deploy.sh
@@ -0,0 +1,6 @@
+kubectl config set-context --current --namespace=bc58
+kubectl apply -f ../mockserver/ms.deployment.yaml
+kubectl apply -f ../mockserver/ms.service.yaml
+kubectl apply -f fe.deployment.yaml
+kubectl apply -f fe.service.yaml
+kubectl apply -f ingress.yaml
