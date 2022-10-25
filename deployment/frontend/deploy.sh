c8da2653473c25450abea981d73127ec2f1d8f98
divya-gariga <divya.gariga@zemosolabs.com>
2022-10-26 02:05:58 +0530

    git sha

--- deployment/frontend/deploy.sh
+++ deployment/frontend/deploy.sh
@@ -1,10 +1,10 @@
 kubectl config set-context --current --namespace=bc58
 
-kubectl set image -f ../mock-server/ms.deployment.yaml mockserver-img=${{ secrets.REGISTRY }}/${{ secrets.REGISTRY_USERNAME }}/bc58-data:${{ github.sha }}
+kubectl set image -f ../mock-server/ms.deployment.yaml mockserver-img=${{ secrets.REGISTRY }}/${{ secrets.REGISTRY_USERNAME }}/bc58-data:$1
 kubectl apply -f ../mock-server/ms.deployment.yaml
 kubectl apply -f ../mock-server/ms.service.yaml
 
-kubectl set image -f fe.deployment.yaml frontend-img=${{ secrets.REGISTRY }}/${{ secrets.REGISTRY_USERNAME }}/bc58-fe:${{ github.sha }}
+kubectl set image -f fe.deployment.yaml frontend-img=${{ secrets.REGISTRY }}/${{ secrets.REGISTRY_USERNAME }}/bc58-fe:$1
 kubectl apply -f fe.deployment.yaml
 kubectl apply -f fe.service.yaml
 
